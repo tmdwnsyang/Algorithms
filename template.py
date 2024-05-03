32a0ec6b663e3fbc0d14ce8c9de8b48de850e2f5
Sam Yang <tmdwns.yang@gmail.com>
2024-05-03 08:23:01 -0700

    Populating Next Right Pointers in Each Node

--- template.py
+++ template.py
@@ -1,6 +1,24 @@
-# Problem: <Problem statement>
-# Link: <link to the problem>
+# Problem: 116. Populating Next Right Pointers in Each Node
+# Link: https://leetcode.com/problems/populating-next-right-pointers-in-each-node/description/
 
 # My Solution:
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        a = []
+        def trav(n: 'Optional[Node]', h:int):
+            nonlocal a
+            if n:
+                if h == len(a):
+                    a.append([])
+                trav(n.left, h + 1)
+                a[h].append(n)
+                trav(n.right, h + 1)
+
+        trav(root, 0)
+
+        for nodes in a:
+            for i in range(len(nodes) - 1):
+                nodes[i].next = nodes[i+1]
+        return root
 
 # Analysis: <Runtime complexity analysis & more...>
