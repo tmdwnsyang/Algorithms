ef00e1e23eebe989bdc43624c9f5b116ca5f4dfb
Sam Yang <tmdwns.yang@gmail.com>
2024-05-04 09:10:45 -0700

    Folder change

---
+++ Trees/zigzagLevelOrder.py
@@ -0,0 +1,22 @@
+# Problem: https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+
+# My Solution
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        a = []
+        def trav(h: int, n: Optional[TreeNode]):
+            nonlocal a
+            if n:
+                if h == len(a): 
+                    a.append(deque())
+                trav(h+1, n.left)
+                if (h % 2) == 1:
+                    a[h].appendleft(n.val)
+                else:
+                    a[h].append(n.val)
+                trav(h+1, n.right)
+        trav(0, root)
+        return a
+
+# Analysis
+# 
