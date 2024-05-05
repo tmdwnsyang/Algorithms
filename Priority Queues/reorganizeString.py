307ad1deb7f4dbd944ddcca40ac5f0d70573ce11
Sam Yang <tmdwns.yang@gmail.com>
2024-05-05 11:07:48 -0700

    Added PQ solution w/o duplicates

--- Priority Queues/reorganizeString.py
+++ Priority Queues/reorganizeString.py
@@ -1,7 +1,7 @@
 # Problem: 767. Reorganize String
 # Link: https://leetcode.com/problems/reorganize-string/description/
 
-# My Solution 1 - Using priority queue (Beats 5.6%)
+# My Solution 1 - Using priority queue with duplicates (Beats 5.6%) 
 class Solution:
     def reorganizeString(self, s: str) -> str:
         m = {}
@@ -26,7 +26,38 @@ class Solution:
         return ans
 
 # Analysis:
-# Runtime: O(n) - 
-# Memory: O(n) - 
+# Runtime: O(nlog(n)) - Where n is the total number of characters
+# Memory: O(n) - Where n is the total number of characters
 # Takeaway - In heapq, the first item in array is the highest priority.
 #            You can use this knowledge to perform a peek.
+
+# Solution 2 - Priority queue without duplicates (with a counter)
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        m = {}
+        pq = [(-i, k) for k, i in Counter(s).items()]
+        a = []
+        heapify(pq)
+        while pq:
+            first_count, first_chr = heappop(pq)
+            if not a or a[-1] != first_chr:
+                a.append(first_chr)
+                if first_count + 1 != 0:
+                    heappush(pq, (first_count + 1, first_chr))
+            else:
+                if not pq:
+                    return ""
+                second_count, second_chr = heappop(pq)
+                a.append(second_chr)
+                if second_count + 1 != 0:
+                    heappush(pq, (second_count + 1, second_chr))
+                heappush(pq, (first_count, first_chr))
+        return ''.join(a)
+
+# Analysis:
+# Runtime: O(n*log(k))
+#          Where n is the total number of characters, and each heapify takes
+#          O(log(k)).     
+# Memory: O(k) - Where k is the total number of unique character in pq.
+# Takeaway - In heapq, the first item in array is the highest priority.
+#            You can use this knowledge to perform a peek in a list.
